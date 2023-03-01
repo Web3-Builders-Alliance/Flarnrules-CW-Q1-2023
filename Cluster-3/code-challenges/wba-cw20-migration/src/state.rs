0359867c4136209927d1d3d1ce05a011f6930559
flarnrules <breifenberg11@gmail.com>
2023-03-01 11:32:57 -0600

    Add more code snippits, modify state.rs

--- Cluster-3/code-challenges/wba-cw20-migration/src/state.rs
+++ Cluster-3/code-challenges/wba-cw20-migration/src/state.rs
@@ -1,1 +1,19 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{Addr, Decimal, Uint128};
+use cw20::Denom;
+use cw_storage_plus::Item;
+
+pub const WBA_CHALLENGE_TOKEN: Item<Addr> = Item::new("wba_challenge_token");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Token {
+    pub reserve: Uint128,
+    pub denom: Denom,
+}
+
+pub const TOKEN1: Item<Token> = Item::new("token1");
+
+pub const OWNER: Item<Option<Addr>> = Item::new("owner");
 
