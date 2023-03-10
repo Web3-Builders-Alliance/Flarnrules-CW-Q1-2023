76658a41dac1ffb6e138f84e6aa9e0ed883277aa
flarnrules <breifenberg11@gmail.com>
2023-03-10 11:17:08 -0600

    Change variable names to be more clear

--- capstone/script.js
+++ capstone/script.js
@@ -2,24 +2,24 @@ const colors = ['#FF5733', '#C70039', '#900C3F', '#581845', '#FFC300', '#FF5733'
 
 const gridContainer = document.querySelector('.grid-container');
 
-for (let i = 0; i < 4; i++) {
+for (let quadrantInstance = 0; quadrantInstance < 4; quadrantInstance++) {
   const quadrant = document.createElement('div');
   quadrant.classList.add('quadrant');
   gridContainer.appendChild(quadrant);
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
           quadrant.style.backgroundColor = randomColor;
 
-  for (let j = 0; j < 16; j++) {
+  for (let coreInstance = 0; coreInstance < 16; coreInstance++) {
     const core = document.createElement('div');
     core.classList.add('core');
     quadrant.appendChild(core);
 
-    for (let k = 0; k < 8; k++) {
+    for (let scapeInstance = 0; scapeInstance < 8; scapeInstance++) {
       const scape = document.createElement('div');
       scape.classList.add('scape');
       core.appendChild(scape);
 
-      for (let l = 0; l < 23; l++) {
+      for (let nodeInstance = 0; nodeInstance < 23; nodeInstance++) {
         const node = document.createElement('div');
         node.classList.add('node');
         scape.appendChild(node);
